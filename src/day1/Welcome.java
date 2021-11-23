f0729fde4c849b93cb31c028847fbf62c040c778
adityabhasupiya <aditya.b.piya@gmail.com>
2021-11-22 22:06:12 -0800

    message changed for greeting again

--- src/day1/Welcome.java
+++ src/day1/Welcome.java
@@ -1,7 +1,7 @@
 package day1;
 public class Welcome {
     public static void main(String[] args) {
-        String greeting = "Welcome to Core Java 2021!";
+        String greeting = "Welcome to Core Java session fourth 2021!";
         System.out.println(greeting);
         for (int i = 0; i < greeting.length(); i++) {
             System.out.print("=");
