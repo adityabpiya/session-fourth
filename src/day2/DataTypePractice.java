2d6c0f2dcb63933532e9036b7ddc1b6e440eff75
adityabhasupiya <aditya.b.piya@gmail.com>
2021-11-25 21:07:40 -0800

    gitignore again

---
+++ src/day2/DataTypePractice.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class DataTypePractice {
+
+    static int g = 9;
+    static int a;
+    static float b;
+    static boolean isTrue;
+    static char d;
+    static String str;
+
+    public static void main(String[] args) {
+
+        System.out.println("a is: " + a);
+        System.out.println("b is: " + b);
+        System.out.println("isTrue is: " + isTrue);
+        System.out.println("g is: " + g);
+        System.out.println("d is: " + d);
+        System.out.println("str is " + str);
+
+
+    }
+}
