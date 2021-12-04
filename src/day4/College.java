f7ecd1f989c188615e0742f81935373499f54764
adityabhasupiya <aditya.b.piya@gmail.com>
2021-12-03 22:55:14 -0800

    Adding list of student and teacher to person test

---
+++ src/day4/College.java
@@ -0,0 +1,23 @@
+package day4;
+
+import java.util.ArrayList;
+
+public class College {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+
+
+        Teacher teacher = new Teacher();
+
+        Person person = new Student();
+        Person person1 = new Teacher();
+
+
+        ArrayList<Person> arrayList = new ArrayList<>();
+        arrayList.add(new Student());
+        arrayList.add(new Teacher());
+
+    }
+
+}
---
+++ src/day4/Teacher.java
@@ -0,0 +1,23 @@
+package day4;
+
+public class Teacher extends Person {
+
+    private double salary;
+    private String subject;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
