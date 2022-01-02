733f8dc2b321462c10de5b41e006f70f9799fec1
adityabhasupiya <aditya.b.piya@gmail.com>
2022-01-02 09:51:49 -0800

    Using enumerated type on items

---
+++ src/day8/EnumPractice.java
@@ -0,0 +1,9 @@
+package day8;
+
+public class EnumPractice {
+
+    public static void main(String[] args) {
+        Item item1 = new Item("Polo Tshirt", 100, Size.SMALL);
+        Item item2 = new Item("Tshirt", 100, Size.LARGE);
+    }
+}
---
+++ src/day8/Item.java
@@ -0,0 +1,14 @@
+package day8;
+
+public class Item {
+
+    private String name;
+    private double price;
+    private Size size;
+
+    public Item(String name, double price, Size size) {
+        this.name = name;
+        this.price = price;
+        this.size = size;
+    }
+}
