a82fd066f0b84e998c78cbaaa59caab0928dc497
adityabhasupiya <aditya.b.piya@gmail.com>
2022-01-12 15:18:56 -0800

    Inheritance test 5.1.6

--- src/day12/inheritance/InheritanceTest.java
+++ src/day12/inheritance/InheritanceTest.java
@@ -4,13 +4,21 @@ public class InheritanceTest {
 
     public static void main(String[] args) {
         Employee employee1 = new Employee("Sireej", 7000);
+
         System.out.println(employee1.toString());
         System.out.println(employee1.getSalary());
 
         Manager manager1 = new Manager("Aditya", 9000);
+
         manager1.setBonus(1000);
+
         System.out.println(manager1.toString());
         System.out.println(manager1.getSalary());
+
+        Employee[] staffs = new Employee[3];
+        Manager boss = new Manager("Aditya", 9000);
+
+        staffs[0] = boss;
     }
 
 }
