f4dcc2b4afbaacd9cd7f8aed69d2f14fc2d147e9
adityabhasupiya <aditya.b.piya@gmail.com>
2022-02-09 14:26:56 -0800

    Assertion test started

---
+++ src/day18/AgeRestrictionException.java
@@ -0,0 +1,8 @@
+package day18;
+
+public class AgeRestrictionException extends RuntimeException{
+
+    public AgeRestrictionException(String message) {
+        super(message);
+    }
+}
--- src/day18/ExceptionPractice.java
+++ src/day18/ExceptionPractice.java
@@ -12,23 +12,38 @@ public class ExceptionPractice {
         throw new RuntimeException("Oops running is throwing an exception");
     }
 
+    public void calculateAgeOfUser(int age) throws AgeRestrictionException {
+        if (age<18){
+            throw new AgeRestrictionException("You are below 18 so you cannot login to our website");
+        }else{
+            System.out.println("You can login to our system");
+        }
+    }
+
     public static void main(String[] args) {
         ExceptionPractice exceptionPractice = new ExceptionPractice();   //we cant call a method here directly coz we can only call static function in static method
 
-        try {
-            exceptionPractice.readFromFile();
-            System.out.println("In between read from file and running");
-            exceptionPractice.running();
-        } catch (IOException e) {
-            System.out.println("In catch block of IOException");
-            e.printStackTrace(); //displays the message of exception .. its a method that is called by exceptions
-        }
+//        try {
+//            exceptionPractice.readFromFile();
+//            System.out.println("In between read from file and running");
+//            exceptionPractice.running();
+//        } catch (IOException e) {
+//            System.out.println("In catch block of IOException");
+//            e.printStackTrace(); //displays the message of exception .. its a method that is called by exceptions
+//        }
 
 //        try {
 //            exceptionPractice.running();
 //        } catch (RuntimeException e){
 //            e.printStackTrace();
 //        }
+        try {
+            exceptionPractice.calculateAgeOfUser(18);
+        }catch (AgeRestrictionException e){
+            System.out.println(e.getLocalizedMessage());
+        }finally {
+            System.out.println("Finally will always execute");
+        }
     }
 
 }
