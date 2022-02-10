2f45c1498f79d3bbb681bec832f509674387cc19
adityabhasupiya <aditya.b.piya@gmail.com>
2022-02-10 14:46:22 -0800

    Dec 25 class update

--- src/day18/AssertionTest.java
+++ src/day18/AssertionTest.java
@@ -1,4 +1,15 @@
 package day18;
 
 public class AssertionTest {
+    public static void main(String[] args) {
+        int x = -2;
+
+//        if (x < 0) {
+//            throw new IllegalArgumentException("x < 0");
+//        }
+
+        assert x >= 0 : "x < 0"; //this replaces the above if statement .... have to be >=0 if not show the message after :
+
+        System.out.println(Math.sqrt(x));
+    }
 }
